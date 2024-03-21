c705043afa9928bf0f3a46f26db7879f84ce5939
anamariaow <new_annemarie@yahoo.com>
2024-03-21 18:43:30 +0100

    swagger dependency

---
+++ src/main/java/com/example/exampleconswagger/ExampleConSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleconswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleConSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleConSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exampleconswagger/ExampleConSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleconswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleConSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
